6aa4c33c02c950301dc45b1a78a163d7b94738dc
UncleSandira <24273@burnside.school.nz>
2026-05-12 11:19:48 +1200

    added a query line and fetch statement

--- print('hello world').py
+++ print('hello world').py
@@ -1,6 +1,11 @@
 import sqlite3
 
 db = sqlite3.connect('Team.db')
-cursor=db.cursor()
+cursor = db.cursor()
 
 playerName = input("Enter player name: ")
+
+cursor.execute("SELECT positionID, positionName, teamID, teamName FROM players WHERE playerName =?", (name,))
+
+results = cursor.fetchone()
+
