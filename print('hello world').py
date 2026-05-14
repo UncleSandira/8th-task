a4fa5305d5bbf0aeedefae3a648e655587cdaca2
UncleSandira <24273@burnside.school.nz>
2026-05-14 12:42:42 +1200

    CHnaged alot

--- print('hello world').py
+++ print('hello world').py
@@ -2,27 +2,51 @@ import sqlite3
 
 db = sqlite3.connect('Team.db')
 cursor = db.cursor()
+print('''Type "Team" if you want to check players in a certain team
+or type "Stats" if you want to chekc a certain players stats''')
 
-playername = input("Enter the player's name: ")
-name = playername.title()
+choose = input('Team/Stats: ').title()
 
-query = """
-SELECT playerID, positionID, teamID FROM player
-WHERE playerName = ?"""
 
-cursor.execute(query, (name,))
-result = cursor.fetchone()
+if choose == "Stats":
+    playername = input("Enter the player's name: ")
+    name = playername.title()
 
-if result is None:
-    print(f"Player '{name}' not found in the database.")
-else:
-    playerID = result[0]
-    positionID = result[1]
-    teamID = result[2]
+    query = """
+    SELECT playerID, positionID, teamID FROM player
+    WHERE playerName = ?"""
+
+    cursor.execute(query, (name,))
+    result = cursor.fetchone()
+
+    if result is None:
+        print(f"Player '{name}' not found in the database.")
+    else:
+        playerID = result[0]
+        positionID = result[1]
+        teamID = result[2]
 
     print(f"Player information for {name}")
     print(f"Player ID: {playerID}")
     print(f"Position ID: {positionID}")
     print(f"Team ID: {teamID}")
+
+elif choose == "Team":
+    teamName = input('Enter name of the team: ')
+    team = teamName.title()
+
+    query2 = """
+    SELECT playerName FROM player
+    Where teamName = ?"""
+
+    cursor.execute(query2, (teamName,))
+    result2 = cursor.fetchall()
+
+    if result2 is None:
+        print(f'{team} not found in the database.')
+    else:
+        print(f"Team matching {team}")
+        for people in result2:
+            print(people[0])
 
 db.close()
